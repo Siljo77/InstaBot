0c5668fc926692407e81bcf49cd080ab13c76342
Siljo77 <ivan.sijan@gmail.com>
2021-12-30 11:32:13 +0100

    notifikacije sada rade uvijek

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from random import randint
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import ui
 
 
 class InstaBot():
@@ -9,7 +13,7 @@ class InstaBot():
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.bot = webdriver.Chrome("C:/Users/IvanSijan/Downloads/chromedriver_win32/chromedriver.exe")
+        self.bot = webdriver.Chrome('/Users/ivansijan/Desktop/chromedriver')
        
     def fullSizeScreen(self):  
         bot =self.bot
@@ -30,26 +34,38 @@ class InstaBot():
     
     def notification_2(self):
         bot = self.bot
-        bot.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button').click()
+        ui.WebDriverWait(bot, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "cmbtv"))).click() 
+        
         
     def notification_3(self):
         bot = self.bot
-        bot.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()    
-        
+        not_3 = bot.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')   
+        not_3.click()
         
     def person(self,person):
         bot = self.bot
         bot.get("https://www.instagram.com/" + person + '/')
-        
+        self.likeFirstPhoto()
+        #self.likePhotos()
         
     def likeFirstPhoto(self):
         bot = self.bot
+        # open a pikcure
         bot.find_element_by_class_name('v1Nh3').click()
         time.sleep(randint(1,3))
-        bot.find_element_by_class_name('fr66n').click()
+        # like the pikcure
+        like = bot.find_element_by_class_name('fr66n')
+        like.click()
         time.sleep(randint(1,3))
+        # click next pikcure
         bot.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div/button').click()
-        
+     
+    def follow(self):
+        bot = self.bot
+        folow = bot.find_element_by_xpath("/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]/button")
+        folow.click()
+        folow_cancle = bot.find_element_by_xpath("/html/body/div[7]/div/div/div/div[3]/button[2]")
+        folow_cancle.click()
         
     def likePhotos(self,amount):
         bot = self.bot
@@ -57,48 +73,57 @@ class InstaBot():
         i = 1
         while i <= amount:
             time.sleep(randint(1,5))
+            # like the pikcure a pikcure
             bot.find_element_by_class_name('fr66n').click()
+            time.sleep(randint(1,2))
+            # folow user
+            self.follow()
+            time.sleep(randint(1,2))
+            # next pikcure
             bot.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div[2]/button').click()
         
             i += 1
             
-        bot.get('https://www.instagram.com/' + 'bogda2709')
+            
         
     def hashtag(self,hashtag):
         bot = self.bot
-        hashtag = ['manwithbeard','longhairdontcare','hockeybenders','cooldads']
+        hashtag = ['pythonprogramming','instabots','bots','vscode'] #'robotica','seleniumwebdriver',
         for has in hashtag:
             bot.get("https://www.instagram.com/explore/tags/" + has + '/')
             time.sleep(randint(1,3))
             self.likeFirstPhoto()
-            self.likePhotos(3)
+            time.sleep(randint(1,2))
+            self.likePhotos(20)
             
-
-
+    
+        bot.get('https://www.instagram.com/' + 'bogda2709')
+        
+    
 insta = InstaBot('robotantonio6','robotantonio66')
 insta.fullSizeScreen()
-time.sleep(randint(1,2))
 
 insta.goToInsta()
-time.sleep(randint(1,3))
+time.sleep(randint(1,2))
 
 insta.notification_1()
 time.sleep(randint(1,3))
 
 insta.login()
-time.sleep(randint(1,3))
+time.sleep(randint(1,5))
 
 insta.notification_2()
 time.sleep(randint(1,3))
 
 insta.notification_3()
 time.sleep(randint(1,3))
-#insta.person('ivan_sijan')
-#time.sleep(randint(1,3))#
+#insta.person('bogda2709')
+#time.sleep(randint(1,3))
 #insta.likeFirstPhoto()
 #time.sleep(randint(1,3))
 #insta.likePhotos(74)
 insta.hashtag('has')
 time.sleep(randint(1,3))
+#time.sleep(randint(1,3))
 
     
